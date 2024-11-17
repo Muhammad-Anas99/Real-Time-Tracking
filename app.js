0d2fdb974b6315d325db560e68d976c2aebf19a6
MuhammadAnas <flashycoderch@gmail.com>
2024-11-17 09:05:30 +0500

    a

--- app.js
+++ app.js
@@ -1,38 +1,25 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const path = require('path')
-const app = express()
-const http = require('http')
-const socketio = require('socket.io')
-const server = http.createServer(app)
-const io = socketio(server)
+const express = require("express");
+const dotenv = require("dotenv").config();
+const path = require("path");
+const app = express();
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app);
+const io = socketio(server);
 const PORT = process.env.PORT || 5000;
- 
 
-app.set('view engine',"ejs") 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.resolve("public")));
 
-io.on("connection",function(socket){
-    console.log('connected!...')
-    socket.on("send-location",function(data){
-        io.emit("receive-location",{id:socket.id, ...data})
-    });
-    socket.on("disconnect",function(){
-        io.emit("user-disconnected",socket.id)
-    })
-})
+io.on("connection", function (socket) {
+  console.log("connected!...");
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+  socket.on("disconnect", function () {
+    io.emit("user-disconnected", socket.id);
+  });
+});
 
-
-app.get('/',(req,res)=>{
-    res.render('index')
-})
-
-
-
-server.listen(PORT,()=>{
-    console.log('Server Started at port : '+ PORT)
-})
-
-
-
-
+server.listen(PORT, () => {
+  console.log("Server Started at port : " + PORT);
+});
